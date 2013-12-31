3435247f411982a76b394d18509268799a89f326
Edwin Villanueva <edwincv0@gmail.com>
2013-12-31 08:10:40 -0800

    added unit tests

--- test/unit/status_test.rb
+++ test/unit/status_test.rb
@@ -6,4 +6,18 @@ class StatusTest < ActiveSupport::TestCase
 		assert !status.save
 		assert !status.errors[:content].empty?
 	end
+
+	test "that a status's content is at least 2 letters" do
+		status = Status.new
+		status.content = "H"
+		assert !status.save
+		assert !status.errors[:content].empty?
+	end
+
+	test "that a status has a user id" do 
+		status = Status.new
+		status.content = "Hello"
+		assert !status.save
+		assert !status.errors[:user_id].empty?
+	end
 end
